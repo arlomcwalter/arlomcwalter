69bb6d9cd9a6b5cb6732397fedd61b328e1901d5
arlomcwalter <mcwalterarlo@gmail.com>
2023-12-16 15:17:00 +0000

    oh

--- .github/script/index.js
+++ .github/script/index.js
@@ -1,16 +1,28 @@
 const fs = require("fs");
 const numeral = require("numeral");
 
-const response = await fetch("https://meteorclient.com/api/stats");
-if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+fetch("https://meteorclient.com/api/stats")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    return res.json();
+  })
+  .then((data) => {
+    if (!data.downloads) {
+      throw new Error("Invalid response data.");
+    }
 
-const data = await response.json();
-if (!data.downloads) throw new Error("Invalid response data.");
+    const downloadCount = parseInt(data.downloads, 10);
+    const downloadString = numeral(downloadCount).format('0.0a');
 
-const downloadCount = parseInt(data.downloads, 10);
-const downloadString = numeral(downloadCount).format('0.0a');
+    // Read template file and replace placeholder
+    const template = fs.readFileSync("../templates/README_TEMPLATE.md", "utf-8");
+    const formatted = template.replace("%METEOR_DOWNLOADS%", downloadString);
 
-const template = fs.readFileSync("../templates/README_TEMPLATE.md", "utf-8");
-const formatted = template.replace("%METEOR_DOWNLOADS%", downloadString);
-
-fs.writeFileSync("../../README.md", formatted, 'utf-8');
+    // Write the formatted content to README.md
+    fs.writeFileSync("../../README.md", formatted, 'utf-8');
+  })
+  .catch((error) => {
+    console.error('Error:', error.message);
+  });
